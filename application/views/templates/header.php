7b47ac2fcdcc8b6bbed06bae889f9facc14d81c8
raven <ravencarlos>
2017-04-05 11:40:44 +0800

    upload

---
+++ application/views/templates/header.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>ciProject</title>
+		<link rel="stylesheet" type="text/css" href="https://bootswatch.com/sandstone/bootstrap.min.css">
+	</head>
+	<body>	
+	<nav class="navbar navbar-inverse">
+		<div class="contatiner">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="/">ravenBlog</a>
+			</div>
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url(); ?>">Home</a></li>
+					<li><a href="<?php echo base_url(); ?>/about">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
